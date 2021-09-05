eccc581d2161fe6593437aa38669b21c23030ffc
Mahadi001 <sadat.himel@gmail.com>
2021-09-05 19:04:32 +0600

    update

---
+++ codestar-demo.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Codestar demo
+Plugin URI: http://example.com/
+Description: Demonstration of CodeStar framework
+Version: 1.0
+Author: sadat hossen himel
+Author URI: http://example.com/
+License: GPLv2 or later
+Text Domain: codestar-demo
+Domain Path: /languages
+*/
+
+function csdemo_assets(){
+    load_plugin_textdomain('codestar-demo',false,plugin_dir_path(__FILE__)."/languages");
+}
+add_action( 'plugin_loaded', 'csdemo_assets');
+
